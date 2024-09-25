e3dc94a17ee4fc0569dd442f60d9d24fb8ff8ca2
Patrick Mayer <patrick.mayer97@hotmail.com>
2024-09-25 07:45:31 -0700

    second test

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World! test one more");
+Console.WriteLine("Hello, World! test one more");
+Console.WriteLine("Hello, World! test one more");
+Console.WriteLine("Hello, World! test one more");
+Console.WriteLine("Hello, World! test one more");
+Console.WriteLine("Hello, World! test one more");
